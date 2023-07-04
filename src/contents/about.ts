40bdaa572723c1bdbfd4307bf44097bc29903434
Shamim Bin Nur <sbnsbn00@gmail.com>
2023-07-04 13:07:45 +0600

    some updates

--- src/contents/about.ts
+++ src/contents/about.ts
@@ -6,29 +6,19 @@ export const aboutUs = {
       description: "Next.js is a widely-used React framework that simplifies the development of modern web applications. It focuses on scalability and production readiness, offering features such as server-side rendering (SSR), static site generation (SSG), and various advanced capabilities. With Next.js, developers can effortlessly build robust and efficient applications powered by React.",
     },
     {
-      slug: "what-this-site-is",
-      title: "What this site is",
+      slug: "about-next.js-13",
+      title: "About Next.js 13",
+      description: "The core feature of Next.js 13 is its enhanced Server Side Rendering capabilities. Next.js 13 is designed to maximize server-side rendering of components, resulting in a significant improvement in website performance. Next.js 13 introduces a new 'App Router' structure in addition to the previous 'Page' directory structure. Every component in the 'App' directory is rendered as a server-side component, which means the developer has to explicitly specify client-side rendering."
+    },
+    {
+      slug: "what-is-this-site-for",
+      title: "What is this site for?",
       description: "This site is an example project built with Next.js 13 to demonstrate the new feature of Next.js 13. This example site is build to showcasing the incredible capabilities of Next.js 13. Explore the power of Next.js 13 and its innovative App Route feature in this hands-on example project. Gain practical insights and unleash your web development potential with our Next.js example project built specifically for Next.js 13",
-    },
-    {
-      slug: "our-mission",
-      title: "Our Mission",
-      description: "Our mission is to empower developers with the knowledge and resources to leverage the latest advancements in Next.js 13 effectively. We strive to create a collaborative community where developers can learn, share, and grow together.",
-    },
-    {
-      slug: "our-values",
-      title: "Our Values",
-      description: "At our core, we believe in transparency, inclusivity, and continuous learning. We value open communication, respect diverse perspectives, and encourage experimentation and improvement."
-    },
-    {
-      slug: "our-history",
-      title: "Our History",
-      description: "Our journey began with a shared passion for web development. Over the years, we have honed our skills, overcome challenges, and embraced new technologies. Our experiences have shaped us into the team we are today, ready to inspire and educate others."
     },
     {
       slug: "our-team",
       title: "Our Team",
-      description: "Meet our talented and dedicated team of developers. Each member brings unique skills and expertise to the table. We collaborate closely, support each other, and share a common goal of delivering outstanding web solutions."
+      description: "You're the team! We are a community of developers who are passionate about web development and Next.js. We are here to learn, share, and grow together."
     }
   ],
 }
